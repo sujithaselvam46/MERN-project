5661a4873fb3596cde0f7d8160c08c546c608b44
Sujitha_05 <8121.cs.sujitha.s@gmail.com>
2026-04-03 17:38:46 +0530

    ui updated

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const router = express.Router();
-const multer = require("multer");
-const { analyzeResume, getAllResumes } = require("../controllers/resumeController");
-const authMiddleware=require("../middleware/authMiddleware")
+  const express = require("express");
+  const router = express.Router();
+  const multer = require("multer");
+  const { analyzeResume, getAllResumes } = require("../controllers/resumeController");
+  const authMiddleware=require("../middleware/authMiddleware")
 
-// Multer setup - Use memory storage for better performance
-const storage = multer.memoryStorage();
+  // Multer setup - Use memory storage for better performance
+  const storage = multer.memoryStorage();
 
-const upload = multer({
-  storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype === "application/pdf") cb(null, true);
-    else cb(new Error("Only PDF resumes are allowed"), false);
-  }
-});
-
-// Routes
-// 1️⃣ Upload route with error handling
-router.post("/upload",authMiddleware, (req, res, next) => {
-  const uploadSingle = upload.single("resume");
-
-  uploadSingle(req, res, function (err) {
-    if (err) {
-      // Multer file type error
-      console.error("Multer error:", err.message);
-      return res.status(400).json({ error: err.message });
+  const upload = multer({
+    storage,
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype === "application/pdf") cb(null, true);
+      else cb(new Error("Only PDF resumes are allowed"), false);
     }
-    next(); // proceed to analyzeResume if file is valid
   });
-}, analyzeResume);
-router.get("/all", authMiddleware,getAllResumes); // must be a function
 
-module.exports = router;
+  // Routes
+  // 1️⃣ Upload route with error handling
+  router.post("/upload",authMiddleware, (req, res, next) => {
+    const uploadSingle = upload.single("resume");
+
+    uploadSingle(req, res, function (err) {
+      if (err) {
+        // Multer file type error
+        console.error("Multer error:", err.message);
+        return res.status(400).json({ error: err.message });
+      }
+      next(); // proceed to analyzeResume if file is valid
+    });
+  }, analyzeResume);
+  router.get("/all", authMiddleware,getAllResumes); // must be a function
+
+  module.exports = router;
